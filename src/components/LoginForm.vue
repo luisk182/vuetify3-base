69438dd7b5db6da4a67fb75b02470adb1fdddfcc
Luis Ledezma <luis.ledezma@axented.com>
2023-04-18 17:22:27 -0600

    Add component files

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-content>
+    <v-snackbar
+      v-model="showErr"
+    >
+      {{ errorMSg }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="showErr = false, errorMSg = ''"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <v-progress-linear :indeterminate="true" color="primary" v-show="loading"></v-progress-linear>
+        <v-card class="elevation-3 pa-6" min-width="400">
+          <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="75"
+        />
+        <v-spacer></v-spacer>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            >
+            <v-text-field
+                solo
+                label="Email"
+                v-model="email"
+                prepend-inner-icon="mdi-email"
+                :rules="emailVal"
+            ></v-text-field>
+
+            <v-text-field
+                solo
+                label="Contraseña"
+                v-model="password"
+                prepend-inner-icon="mdi-lock"
+                :rules="passVal"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block color="blue" @click="validate()" :disabled="loading" dark>Iniciar sesón</v-btn>
+        </v-card-actions>
+        <div>
+          <v-btn text block :disabled="loading" @click="$emit('change-login')">¿Olvidaste tu contraseña?</v-btn>
+        </div>
+        <hr>
+      </v-card>
+    <v-dialog v-model="dialog" persistent max-width="800px">
+      <template>
+        <v-card>
+          <v-card-title>
+            <div style="margin: auto;" class="headline">{{msg}}</div>
+          </v-card-title>
+          <v-card-text>
+            <div v-html="link1"></div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="dialog = false">Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </v-content>
+</template>
+
+<script lang="ts">
+  import { defineComponent, onMounted, ref } from "vue";
+
+  export default defineComponent({
+    name: 'LoginForm',
+    setup(){
+      onMounted(() => {
+        console.log("woomy");
+      });
+    }
+  });
+
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="fill-height bg-grey-lighten-4" fluid>
+    <v-row algin="center" justify="center">
+        <div class="login-logo">
+        </div>
+        <LoginForm />
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import LoginForm from "../components/LoginForm.vue";
+
+export default defineComponent({
+  name: "Login",
+  components: {
+    LoginForm,
+  },
+  setup(){
+    onMounted(() => {
+      console.log("woomy");
+    });
+
+  },
+});
+
+</script>
+<style style="scss">
+.v-progress-linear {
+  margin:0 !important;
+}
+</style>
