e6c068576e6ef3978e6544486684491bfd35b7af
Luis Ledezma <luis.ledezma@axented.com>
2023-04-19 18:00:26 -0600

    Add snackbar toaster component

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,20 +1,6 @@
 <template>
   <v-content>
-    <v-snackbar
-      v-model="showErr"
-    >
-      {{ errorMSg }}
-      <template v-slot:action="{ attrs }">
-        <v-btn
-          color="pink"
-          text
-          v-bind="attrs"
-          @click="showErr = false, errorMSg = ''"
-        >
-          Close
-        </v-btn>
-      </template>
-    </v-snackbar>
+    <Toaster v-model="showMsg" :message="`some error message`" />
     <v-progress-linear :indeterminate="true" color="primary" v-show="loading"></v-progress-linear>
         <v-card class="elevation-3 pa-6" min-width="400">
           <v-img
@@ -48,41 +34,43 @@
           </v-form>
         </v-card-text>
         <v-card-actions>
-          <v-btn block color="blue" @click="validate()" :disabled="loading" dark>Iniciar sesón</v-btn>
+          <v-btn block color="blue" @click="validate" :disabled="loading" dark>Iniciar sesón</v-btn>
         </v-card-actions>
         <div>
           <v-btn text block :disabled="loading" @click="$emit('change-login')">¿Olvidaste tu contraseña?</v-btn>
         </div>
         <hr>
       </v-card>
-    <v-dialog v-model="dialog" persistent max-width="800px">
-      <template>
-        <v-card>
-          <v-card-title>
-            <div style="margin: auto;" class="headline">{{msg}}</div>
-          </v-card-title>
-          <v-card-text>
-            <div v-html="link1"></div>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="dialog = false">Cerrar</v-btn>
-          </v-card-actions>
-        </v-card>
-      </template>
-    </v-dialog>
   </v-content>
 </template>
 
 <script lang="ts">
   import { defineComponent, onMounted, ref } from "vue";
+  import Toaster from "@/components/Toaster.vue";
 
   export default defineComponent({
     name: 'LoginForm',
+    components: {
+      Toaster,
+    },
     setup(){
+      const showMsg = ref(false);
+      const loading = ref(false);
       onMounted(() => {
-        console.log("woomy");
+        setTimeout(() => {
+          showMsg.value = true;
+      }, 1000);
       });
+
+      const validate = () => {
+        console.log("some")
+      }
+
+      return {
+        validate,
+        showMsg,
+        loading,
+      }
     }
   });
 
---
+++ src/components/Toaster.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-snackbar v-model="modelValue">
+    {{ message }}
+  <template v-slot:actions>
+    <v-btn
+      color="pink"
+      text
+      @click="closeSnackBar"
+    >
+      Cerrar
+    </v-btn>
+  </template>
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, onMounted } from "vue";
+
+  export default defineComponent({
+    name: 'Toaster',
+    emits: ['update:modelValue'],
+    props:{
+      message: {
+        type: String,
+        default: "This is an alert message",
+      },
+      variant: {
+        type: String,
+        default: "default"
+      },
+      alertType: {
+        type: String,
+        default: "text",
+      },
+      show: {
+        type: Boolean,
+      },
+      modelValue: Boolean,
+    },
+    setup(props, context){
+      onMounted(()=>{
+        console.log(props)
+      })
+      const closeSnackBar = () => {
+        context.emit("update:modelValue", false)
+      }
+
+      return{ 
+        closeSnackBar,
+      }
+    }
+  });
+
+</script>
